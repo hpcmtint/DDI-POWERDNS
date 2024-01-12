a22f41fa9ddf6c0278d647ef2583983313e8db6d
HPC MTINT <142393927+hpcmtint@users.noreply.github.com>
2024-01-12 13:41:41 +0000

    Create bookmarks.m

---
+++ bookmarks.m
@@ -0,0 +1,48 @@
+DDI with PowerDNS/BIND
+
+https://stork.readthedocs.io/en/v1.14.0/
+
+BIND/DNS/POWERDNS
+DDI with PowerDNS/BIND
+BIND
+GitHub - diddi-/phpipam2bind: Script to update BIND servers with data from phpipam
+How To Configure BIND as a Private Network DNS Server on Ubuntu 14.04 | DigitalOcean
+STOCK
+GitHub - isc-projects/stork: Stork is a dashboard for BIND 9 and Kea DHCP. It is under active development, with monthly releases. The initial priority is to provide monitoring and troubleshooting support.
+ISC Open Source Projects / stork · GitLab
+Stork Administrator Reference Manual — Stork 1.14.0 documentation
+(183) Integration powerdns to phpipam - YouTube
+Bind vs dnsmasq vs PowerDNS vs Unbound | ComputingForGeeks
+DNS server types | Cloudflare
+domain name system - PowerDNS Split Horizon Resolver - Server Fault
+Dynamic DNS Update (RFC 2136) — PowerDNS Authoritative Server documentation
+Etzeitet/powerdns-recursor-docker: A docker image for powerdns recursor with forwarding to Authoritative DNS and Pi-Hole.
+Generic MySQL backend — PowerDNS Authoritative Server documentation
+Generic SQLite 3 backend — PowerDNS Authoritative Server documentation
+GitHub - Newlode/phpipam: phpIPAM - IP address management in Docker container
+GitHub - rglaue/ddi-phpipam: DDI - phpIPAM - DNS, DHCP, and IPAM in Docker containers
+GitHub - wargus85/phpIPAM_PDNS_Scripts: Scripts for interacting with phpIPAM and PowerDNS Admin
+GOOD INSTALL GUIDE Provisioning DNS Server Using PowerDNS - Devcrutch.com
+How to Install PowerDNS and PowerDNS-Admin on Ubuntu 22.04
+How to Setup Dynamic DNS (DDNS) Using Kea and Bind on Debian or Ubuntu | Tech Tutorials
+hpcmtint/docker-powerdns: PowerDNS docker container
+hpcmtint/powerdns-recursor-docker: A docker image for powerdns recursor with forwarding to Authoritative DNS and Pi-Hole.
+Implementing BIND Views with PowerDNS
+Install PowerDNS on Ubuntu 18.04, 20.04, & 22.04 | phoenixNAP KB
+Installing PowerDNS (With MySQL Backend) And Poweradmin On Debian Squeeze
+interlegis/powerdns - Docker Image
+IPAM: A cli based IP Address Management tool
+Launching Docker Container with Non-Root User | by Famidha Thurab | Make Android | Jan, 2024 | Medium
+Molnár Péter's Professional Blog - Migration from BIND/OpenDNSSEC to PowerDNS with DNSSEC
+MySQL BIND SDB Driver
+pdns/docker-compose.yml at master · PowerDNS/pdns · GitHub
+PowerDNS and importing from Bind to MySQL | Linux Puzzles
+PowerDNS Authoritative Server — PowerDNS Authoritative Server documentation
+PowerDNS integration should rather use the PowerDNS JSON API · Issue #302 · phpipam/phpipam · GitHub
+powerdns-docker/docker-compose.yml at main · uhernfr/powerdns-docker
+pschiffe/pdns-recursor - Docker Image
+Running PowerDNS in the local environment with PowerDNS-Admin | by Francisco Hernando | Medium
+Stork Management Dashboard for Kea DHCP - YouTube
+The Portal - Home
+zone2sql — PowerDNS Authoritative Server documentation
+CEPH
